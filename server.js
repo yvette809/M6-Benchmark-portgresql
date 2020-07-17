f2c497e23695edd1e45757bd73881099c3c67607
Yvette <nchombuayvta@gmil.com>
2020-07-17 09:12:26 +0200

    initial files and routes created

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const db = require ('./src/db')
+const prodRouter = require('./src/routes/products')
+const cors = require("cors")
+const dotenv = require("dotenv")
+const reviewsRouter = require("./src/routes/reviews")
+
+dotenv.config()
+
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+const port = process.env.PORT || 3005
+
+server.get('/testSQL', async(req,res)=>{
+    const response = await db.query("SELECT 1 +1")
+    console.log('response is',response)
+    res.send(response)
+})
+server.use('/students', studRouter)
+server.use('/projects',projRouter)
+
+
+
+server.listen(port, ()=>{
+    console.log(`server running on port ${port}`)
+})
